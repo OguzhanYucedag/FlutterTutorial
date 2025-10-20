22123b0b23b490ad7a5b0e3a123b53ed8e14448c
Oğuzhan Yücedağ <oguzhanyucedagg@gmail.com>
2025-10-20 23:38:36 +0300

    Update main.dart

--- flutterProjelerim/tutorial_project/lib/main.dart
+++ flutterProjelerim/tutorial_project/lib/main.dart
@@ -17,13 +17,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Başlık'),
           ),         
-        body: Text('Merhaba Dünya'),
+        body: Text(
+          'Merhaba Dünya',
+          style: TextStyle(
+            color: Colors.red, fontSize: 24, fontWeight: FontWeight.w500
+          ),
+          ),
         floatingActionButton: FloatingActionButton(//bu buton standart olarak uygulamanı sağ alt tarafında gözükür
           onPressed: () {//onPressed anlamı geriye herhangi  bir değer döndürmeyen bir parametre
             debugPrint('Tıklandı');
           },
           backgroundColor: Colors.red,
-          child: Text('e'),//child metodu her zaman FloatingActionButtonun en sonunda yer almalıdır aksi takdirde uyarı verir 
+          child: Icon(Icons.account_circle_rounded,
+          color: Colors.black,
+          ),//child metodu her zaman FloatingActionButtonun en sonunda yer almalıdır aksi takdirde uyarı verir 
         ),
       ),
     );
